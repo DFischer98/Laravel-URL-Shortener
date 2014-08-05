d46b4ec9a80038b0defaf932e49a5c758c47ae52
Daniel Fischer <danielalanfischer@gmail.com>
2014-08-04 22:45:29 -0400

    Styling tweaks

--- app/views/navbar.blade.php
+++ app/views/navbar.blade.php
@@ -8,13 +8,13 @@
 		@if(Auth::check())
 			<li><a href='/my-account'><span class = 'underline'>My Account</span></a></li>
 			
-			<li><a href='/logout'><span class = 'underline'>Log out</span></a></li>
+			<li><a href='/logout'><span class = 'underline'>Log Out</span></a></li>
 						
 		@else 	
 			
-			<li><a href='/signup'><span class = 'underline'>Sign up</span></a></li>
+			<li><a href='/signup'><span class = 'underline'>Sign Up</span></a></li>
 
-			<li><a href='/login'><span class = 'underline'>Log in</span></a></li>
+			<li><a href='/login'><span class = 'underline'>Log In</span></a></li>
 		@endif	
 	</ul>
 </div>
